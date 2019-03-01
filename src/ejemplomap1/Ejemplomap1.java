c47e3a7ce1a2fe6efae0c0bacf79a4110a3a2d02
NathaliaRodriguez <nathalia_c.rodriguez>
2019-03-01 16:50:35 -0500

    xml
    se crea una clase para crear archivos XML

--- src/ejemplomap1/Ejemplomap1.java
+++ src/ejemplomap1/Ejemplomap1.java
@@ -11,9 +11,9 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.TreeMap;
+import javax.swing.JOptionPane;
 import modelo.Cliente;
-
-
+import modelo.FileXML;
 
 /**
  *
@@ -25,25 +25,26 @@ public class Ejemplomap1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        LinkedList<Cliente> listac = new LinkedList<>();
+        LinkedList<Cliente> listac2 = new LinkedList<>();
+        Map<Integer, LinkedList<Cliente>> map1 = new HashMap<>();
         
-        LinkedList<Cliente>listac = new LinkedList<>();
-        LinkedList<Cliente>listac2 = new LinkedList<>();
-        Map <Integer, LinkedList<Cliente>> map1 = new HashMap<>();
-        listac.add( new Cliente ("David", "1130", "Uao"));
-        listac.add( new Cliente ("Sofia", "1131", "Tigo"));
-        listac.add( new Cliente ("Marcos", "1132", "Sura"));
-        listac.add( new Cliente ("Maria", "1133", "Herbalife"));
-        map1.put(3,listac);
-        
-        listac2.add( new Cliente ("Orlando", "1136", "Campo"));
-        listac2.add( new Cliente ("Antonio", "1137", "Casa"));
-        listac2.add( new Cliente ("Alejandro", "1138", "Emcali"));
-        listac2.add( new Cliente ("Carlos", "1139", "Javeriana"));
-        map1.put(6,listac2);
-        
-        Map <Integer, Cliente> map2 = new LinkedHashMap<>();
-        Map <Integer, Cliente> map3 =new TreeMap<>();
-        
+        listac.add(new Cliente("David", "1130", "Uao"));
+        listac.add(new Cliente("Sofia", "1131", "Tigo"));
+        listac.add(new Cliente("Marcos", "1132", "Sura"));
+        listac.add(new Cliente("Maria", "1133", "Herbalife"));
+        map1.put(3, listac);
+
+        listac2.add(new Cliente("Orlando", "1136", "Campo"));
+        listac2.add(new Cliente("Antonio", "1137", "Casa"));
+        listac2.add(new Cliente("Alejandro", "1138", "Emcali"));
+        listac2.add(new Cliente("Carlos", "1139", "Javeriana"));
+        map1.put(6, listac2);
+
+        Map<Integer, Cliente> map2 = new LinkedHashMap<>();
+        Map<Integer, Cliente> map3 = new TreeMap<>();
+
 //        map1.put (55, new Cliente ("David", "1130", "Uao"));
 //        map1.put (4, new Cliente ("Sofia", "1131", "Tigo"));
 //        map1.put (5, new Cliente ("Marcos", "1132", "Sura"));
@@ -58,7 +59,6 @@ public class Ejemplomap1 {
 //        map3.put (45, new Cliente ("Sofia", "1131", "Tigo"));
 //        map3.put (5, new Cliente ("Marcos", "1132", "Sura"));
 //        map3.put (78, new Cliente ("Maria", "1133", "Herbalife"));
-        
         String m1 = mostrarDatosMap(map1);
 //        String m2 = mostrarDatosMap(map2);
 //        String m3 = mostrarDatosMap(map3);
@@ -67,8 +67,18 @@ public class Ejemplomap1 {
 //        System.out.println(m2);
 //        System.out.println("_______________");
 //        System.out.println(m3);
+
+        boolean t = FileXML.crearArchivoXML(map1);
+
+        if (t) {
+            JOptionPane.showMessageDialog(null, "Archivo creado con exito");
+        } else {
+            JOptionPane.showMessageDialog(null, "Archivo creado con exito");
+        }
     }
-    public static String mostrarDatosMap (Map <Integer, LinkedList<Cliente>> m){
+
+
+public static String mostrarDatosMap (Map <Integer, LinkedList<Cliente>> m){
         String mostrar = "";
         Iterator<Map.Entry<Integer,LinkedList<Cliente>>> it = m.entrySet().iterator();
         
